4a232f1740d897a970952f0601c3974f67f639fa
Adiego <diih_augusto@live.com>
2016-07-13 15:05:50 -0300

    Add project files.

---
+++ CodingCraft-04/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CodingCraft_04.MvcApplication" Language="C#" %>
---
+++ CodingCraft-04/infraestrcture/Membership/CustomMembershipUser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace CodingCraft_04.infraestrcture.Membership
+{
+    public class CustomMembershipUser : MembershipUser
+    {
+
+
+        public string Name { get; set; }
+
+        public CustomMembershipUser(
+            string providername,
+            string username,
+            object providerUserKey,
+            string email,
+            string passwordQuestion,
+            string comment,
+            bool isApproved,
+            bool isLockedOut,
+            DateTime creationDate,
+            DateTime lastLoginDate,
+            DateTime lastActivityDate,
+            DateTime lastPasswordChangedDate,
+            DateTime lastLockedOutDate,
+            string name) :
+            base(providername,
+                username,
+                providerUserKey,
+                email,
+                passwordQuestion,
+                comment,
+                isApproved,
+                isLockedOut,
+                creationDate,
+                lastLoginDate,
+                lastPasswordChangedDate,
+                lastActivityDate,
+                lastLockedOutDate)
+        {
+
+            Name = name;
+
+        }
+    }
+
+
+}
